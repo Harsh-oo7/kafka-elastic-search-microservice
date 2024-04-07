621afd54013994988c27effc52bdd1c7242e3c8c
Harsh-oo7 <hkaneria22@gmail.com>
2024-04-07 13:04:41 +0530

    Add catalog service and jest

---
+++ catalog_service/src/api/catalog.routes.ts
@@ -0,0 +1,12 @@
+import express, { Response, Request, NextFunction } from "express";
+
+const router = express.Router();
+
+router.post(
+  "/product",
+  async (req: Request, res: Response, next: NextFunction) => {
+    return res.status(200).json({});
+  }
+);
+
+export default router;
---
+++ catalog_service/src/repository/catalog.repository.ts
@@ -0,0 +1,23 @@
+import { ICatalogRepository } from "../interface/catalogRepository.interface";
+import { Product } from "../models/product.model";
+
+// Repository is only responsible for data operation on DB.
+export class CatalogRepository implements ICatalogRepository {
+    create(data: Product): Promise<Product> {
+        throw new Error("Method not implemented.");
+    }
+    update(data: Product): Promise<Product> {
+        throw new Error("Method not implemented.");
+    }
+    delete(id: any) {
+        throw new Error("Method not implemented.");
+    }
+    find(): Promise<Product[]> {
+        throw new Error("Method not implemented.");
+    }
+    findOne(id: number): Promise<Product> {
+        throw new Error("Method not implemented.");
+    }
+    
+
+}
