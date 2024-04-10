0a99be05243953632cf00c2705584e5a556e1b4b
Harsh-oo7 <hkaneria22@gmail.com>
2024-04-10 20:27:45 +0530

    Add test cases for product

--- catalog_service/README.md
+++ catalog_service/README.md
@@ -1,4 +1,5 @@
 
 ## some commands
 1. npm init jest@latest
+2. npm run test  -> to run test cases
 
--- catalog_service/src/services/catalog.service.ts
+++ catalog_service/src/services/catalog.service.ts
@@ -7,13 +7,33 @@ export class CatalogService {
     this._repository = repository;
   }
 
-  createProduct(input: any) {}
+  async createProduct(input: any) {
+
+    const data = await this._repository.create(input);
+    if(!data.id) {
+      throw new Error("unable to create product")
+    }
+    return data;
+  }
   
-  updateProduct(input: any) {}
+  async updateProduct(input: any) {
+    const data = await this._repository.update(input)
 
-  getProducts(limit: number, offset: number) {}
+    return data;
+  }
 
-  getProduct(id: number) {}
+  async getProducts(limit: number, offset: number) {
+    const products = await this._repository.find(limit, offset)
+    return products;
+  } 
 
-  deleteProduct(id: number) {}
+  async getProduct(id: number) {
+    const product = await this._repository.findOne(id)
+    return product;
+  }
+
+  async deleteProduct(id: number) {
+    const response = await this._repository.delete(id)
+    return response;
+  }
 }
